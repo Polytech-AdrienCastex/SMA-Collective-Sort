f6f23cab1b379a502000713b398a90f8e1b56b0c
bvuillemin <benoit.vuil@gmail.com>
2015-12-14 17:26:56 +0100

    MAJ d'Agent

--- SMA-Collective-Sort/src/sma/model/Agent.java
+++ SMA-Collective-Sort/src/sma/model/Agent.java
@@ -1,10 +1,33 @@
 package sma.model;
 
-public class Agent implements Runnable
-{
+import java.util.LinkedList;
+
+public class Agent implements Runnable {
+    private CarriableObject carriableObject;
+    private LinkedList<CarriableObject> history;
+
+    public Agent() {
+        history = new LinkedList<>();
+    }
+
+    public CarriableObject getCarriableObject() {
+        return carriableObject;
+    }
+
+    public boolean setCarriableObject(CarriableObject carriableObject) {
+        if (this.carriableObject == null) {
+            this.carriableObject = carriableObject;
+            return true;
+        }
+        return false;
+    }
+
+    public void removeCarriableObject() {
+        this.carriableObject = null;
+    }
+
     @Override
-    public void run()
-    {
-        
+    public void run() {
+
     }
 }
