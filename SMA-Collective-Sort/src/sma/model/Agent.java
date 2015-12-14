ca1972ff98268471eb5ad25661ad7877dd343e5c
bvuillemin <benoit.vuil@gmail.com>
2015-12-14 18:02:35 +0100

    MAJ d'Agent

--- SMA-Collective-Sort/src/sma/model/Agent.java
+++ SMA-Collective-Sort/src/sma/model/Agent.java
@@ -1,13 +1,25 @@
 package sma.model;
 
+import sma.model.grid.Case;
+import sma.model.grid.Grid;
+
+import java.util.Collection;
 import java.util.LinkedList;
 
 public class Agent implements Runnable {
+    private final static int MAX_HISTORY = 10;
+    private final static double K_PLUS = 0.1;
+    private final static double K_MINUS = 0.3;
+
     private CarriableObject carriableObject;
     private LinkedList<CarriableObject> history;
+    private int number_of_objects_history;
+    private Grid grid;
 
-    public Agent() {
+    public Agent(Grid g) {
         history = new LinkedList<>();
+        number_of_objects_history = 0;
+        grid = g;
     }
 
     public CarriableObject getCarriableObject() {
@@ -24,6 +36,27 @@ public class Agent implements Runnable {
 
     public void removeCarriableObject() {
         this.carriableObject = null;
+    }
+
+    public void updateHistory(CarriableObject c) {
+        history.addLast(c);
+        if (c != CarriableObject.EMPTY) {
+            number_of_objects_history++;
+        }
+        while (history.size() > MAX_HISTORY) {
+            if (history.removeFirst() != CarriableObject.EMPTY) {
+                number_of_objects_history--;
+            }
+        }
+    }
+
+    public double computeFp() {
+        return number_of_objects_history / (double) history.size();
+    }
+
+    public double computeFd(CarriableObject c) {
+        Collection<Case> collection = this.grid.getLocalCases(this);
+        return collection.stream().map(Case::getObject).mapToInt(o -> o == c ? 1 : 0).average().getAsDouble();
     }
 
     @Override
